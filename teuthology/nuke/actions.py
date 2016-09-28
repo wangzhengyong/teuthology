ac0809db065de8018b792c4de90fbbff74d186f8
John Spray <john.spray@redhat.com>
2016-09-28 17:23:29 +0100

    teuthology: update nuke for ceph-mgr
    Signed-off-by: John Spray <john.spray@redhat.com>

--- teuthology/nuke/actions.py
+++ teuthology/nuke/actions.py
@@ -53,6 +53,7 @@ def shutdown_daemons(ctx):
                 'ceph-mon',
                 'ceph-osd',
                 'ceph-mds',
+                'ceph-mgr',
                 'ceph-fuse',
                 'ceph-disk',
                 'radosgw',
@@ -233,7 +234,7 @@ def remove_ceph_packages(ctx):
                                'libcephfs1', 'librados2', 'librgw2', 'librbd1',
                                'ceph-selinux', 'python-cephfs', 'ceph-base',
                                'python-rbd', 'python-rados', 'ceph-mds',
-                               'libcephfs-java', 'libcephfs-jni',
+                               'ceph-mgr', 'libcephfs-java', 'libcephfs-jni',
                                'ceph-deploy', 'libapache2-mod-fastcgi'
                                ]
     pkgs = str.join(' ', ceph_packages_to_remove)
